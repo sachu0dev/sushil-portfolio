11d6d1891dcd96a8124a320aed82a53a7299821b
sachu0dev <sushil.dev.in@gmail.com>
2024-10-25 16:57:41 +0530

    EH: mobile

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
         className="flex flex-col h-full"
       >
         <div className="flex flex-col mb-10 xl:mb-0 justify-center items-center ">
-          <div className="flex space-x-10  items-center md:pr-16 xl:pr-32  sm:mb-2 xl:mb-14">
+          <div className="flex space-x-4 sm:space-x-10  items-center md:pr-16 xl:pr-32  sm:mb-2 xl:mb-14">
             <h1 className="text-3xl text-center md:text-6xl xl:text-8xl font-KGBlankSpaceSketch">
               SUSHIL KUMAR
             </h1>
@@ -25,10 +25,10 @@ const page = () => {
               animate={{ x: 0, opacity: 1 }}
               transition={{ delay: 0.4, duration: 0.5 }}
             >
-              <Smile size={60} color="#FFD700" />
+              <Smile className=" text-[50px] sm:text-[60px]" color="#FFD700" />
             </motion.div>
           </div>
-          <div className="flex relative space-x-10 transform items-center">
+          <div className="flex relative mb-10 md:mb-0 space-x-4 sm:space-x-10 transform items-center">
             <motion.div
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -58,7 +58,7 @@ const page = () => {
         <div className="flex justify-between flex-col-reverse xl:flex-row mt-0 xl:mt-[200px]">
           {/* twitter and linkedin */}
           <motion.div
-            className="flex space-x-2 items-center mt-20 xl:mt-0 pl-16 md:pl-0"
+            className="flex space-x-2 items-center mt-20 xl:mt-0 pl-6 sm:pl-10 md:pl-0"
             initial={{ x: -50, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ delay: 0.4, duration: 0.5 }}
@@ -84,14 +84,14 @@ const page = () => {
             initial={{ y: 50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.8, duration: 0.3 }}
-            className="flex space-x-12 items-center xl:items-center flex-col-reverse md:flex-row-reverse"
+            className="flex sm:space-x-12 items-center flex-row-reverse"
           >
             <Link href="mailto:sushil.dev.in@gmail.com" target="_blank">
-              <Button className="text-lg md:text-xl p-6 md:p-8 hover:bg-gray-300 bg-white text-black font-extrabold">
+              <Button className=" text-xs sm:text-lg md:text-xl p-2 mx-8 sm:p-6 md:p-8 hover:bg-gray-300 bg-white text-black font-extrabold">
                 Contect Me
               </Button>
             </Link>
-            <p className="md:w-[500px] mb-3 md:mb-0 px-8 text-wrap text-left font-roboto ">
+            <p className=" text-xs sm:text-xl sm:w-[500px] mb-3 md:mb-0 pl-8 sm:px-8 text-wrap text-left font-roboto ">
               Hello, I&apos;m Sushil Kumar, a full-stack developer from India,
               proficient in MERN and Next.js, with expertise in crafting
               efficient APIs and beautiful websites, leveraging SQL and NoSQL
